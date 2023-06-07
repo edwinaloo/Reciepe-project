e059a0f958b849d965c383d7eeff97696e8a5872
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-06-08 00:43:02 +0300

    added review feature

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ function searchRecipes() {
         }
     }
 }
+function submitReview(event) {
+    event.preventDefault();
+
+    const nameInput = document.getElementById('reviewNameInput');
+    const contentInput = document.getElementById('reviewContentInput');
+    const ratingInput = document.getElementById('reviewRatingInput');
+    const reviewsList = document.getElementById('reviewsList');
+
+    const name = nameInput.value;
+    const content = contentInput.value;
+    const rating = ratingInput.value;
+
+    const reviewItem = document.createElement('li');
+    reviewItem.innerHTML = `
+        <div class="review-author">${name}</div>
+        <div class="review-rating">${rating}</div>
+        <div class="review-content">${content}</div>
+    `;
+
+    reviewsList.appendChild(reviewItem);
+
+    nameInput.value = '';
+    contentInput.value = '';
+    ratingInput.value = '';
+}
